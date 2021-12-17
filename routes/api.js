34e58acfdc2964705077295794e6abf0faf0f0b1
TESFALEM HAILESLASSIE TEGENGE <33948864+tesfumfa@users.noreply.github.com>
2021-12-17 10:29:37 -0500

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+/* get the list of books */
+router.get('/books', function (req, res, next) {
+ const query = req.query ? req.query : {}
+  Book.find(query, function (err, books) {
+    if (err) {
+      next(err)
+    } else {
+      res.json(books)
+    }
+  })
+})
+
+/* create a new book */
+router.post('/books', function (req, res, next) {
+  Book.create(req.body, function (err, book) {
+    if (err) {
+      next(err)
+    } else {
+      res.json(book)
+    }
+  })
+  /* or */
+  // Book.create(req.body).then(function (err, book) {
+  //   if (err) {
+  //     console.log(err)
+  //   } else {
+  //     res.json(book)
+  //   }
+  // }).catch(next)
+})
+
+/* update book */
+router.put('/books/:id', function (req, res, next) {
+  Book.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, book) {
+    if (err) {
+      next(err)
+    } else {
+      res.json(book)
+    }
+  })
+})
+
+/* delete book */
+router.delete('/books/:id', function (req, res, next) {
+  Book.findByIdAndRemove(req.params.id, function (err, book) {
+    if (err) {
+      next(err)
+    } else {
+      res.json(book)
+    }
+  })
+})
+
+module.exports = router
